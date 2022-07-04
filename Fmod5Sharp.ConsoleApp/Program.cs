cf89d3b0242aefd4a46a87f7f36599deb76377fb
Jeremy Pritts <jrpritts7@gmail.com>
2022-07-04 16:32:34 -0400

    initial changes to switch

---
+++ Fmod5Sharp.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Fmod5Sharp.FmodTypes;
+using Fmod5Sharp.Util;
+
+namespace Fmod5Sharp.ConsoleApp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			if (args.Length != 1)
+			{
+				Console.WriteLine("This program takes exactly one argument: the path to an fsb file.");
+				return;
+			}
+
+			string path = args[0];
+			
+			if (!File.Exists(path))
+			{
+				Console.WriteLine($"No file exists at {path}");
+				return;
+			}
+
+			byte[] rawData = File.ReadAllBytes(path);
+			FmodSoundBank fsbData = FsbLoader.LoadFsbFromByteArray(rawData);
+			FmodAudioType audioType = fsbData.Header.AudioType;
+			if (audioType.IsSupported() && fsbData.Samples.Single().RebuildAsStandardFileFormat(out var decodedData, out var fileExtension))
+			{
+				File.WriteAllBytes("output." + fileExtension, decodedData);
+				Console.WriteLine("Done!");
+				return;
+			}
+			Console.WriteLine($"Failed to convert audio ({audioType})");
+		}
+	}
+}
--- Fmod5Sharp/Util/FmodVorbisData.cs
+++ Fmod5Sharp/Util/FmodVorbisData.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Linq;
+﻿using Sewer56.BitStream;
+using Sewer56.BitStream.ByteStreams;
+using System;
+using System.Text;
 using System.Text.Json.Serialization;
-using BitStreams;
 
 namespace Fmod5Sharp.Util;
 
@@ -31,49 +32,61 @@ internal class FmodVorbisData
 
         _initialized = true;
         
-        var bitStream = new BitStream(HeaderBytes);
+        var bitStream = new BitStream<ArrayByteStream>(new ArrayByteStream(HeaderBytes));
 
-        if (bitStream.ReadByte() != 5) //packing type 5 == books
+        if (bitStream.Read8(8) != 5) //packing type 5 == books
             return;
 
-        if (bitStream.ReadString(6) != "vorbis") //validate magic
+        if (ReadString(bitStream, 6) != "vorbis") //validate magic
             return;
 
         //Whole bytes, bit remainder
-        bitStream.Seek(SeekBit / 8, SeekBit % 8);
+        bitStream.Seek(SeekBit / 8, (byte)(SeekBit % 8));
 
         //Read 6 bits and add one
-        var numModes = bitStream.ReadByte(6) + 1; 
+        var numModes = bitStream.Read8(6) + 1;
 
         //Read the first bit of each mode and skip the rest of the mode data. These are our flags.
-        BlockFlags = Enumerable.Range(0, numModes).Select(_ =>
+        BlockFlags = new byte[numModes];
+		for (int i = 0; i < numModes; i++)
         {
-            var flag = (byte)bitStream.ReadBit();
+			var flag = bitStream.ReadBit();
 
-            //Skip the bits we don't care about
-            bitStream.ReadBits(16);
-            bitStream.ReadBits(16);
-            bitStream.ReadBits(8);
+			//Skip the bits we don't care about
+			bitStream.Read16(16);
+			bitStream.Read16(16);
+			bitStream.Read8(8);
 
-            return flag;
-        }).ToArray();
+			BlockFlags[i] = flag;
+		}
     }
 
     public int GetPacketBlockSize(byte[] packetBytes)
     {
-        var bitStream = new BitStream(packetBytes);
+        var bitStream = new BitStream<ArrayByteStream>(new ArrayByteStream(packetBytes));
 
-        if (bitStream.ReadBit())
+        if (bitStream.ReadBit() != 0)
             return 0;
 
         var mode = 0;
 
         if (BlockFlags.Length > 1)
-            mode = bitStream.ReadByte(BlockFlags.Length - 1);
-
-        if (BlockFlags[mode] == 1)
+            mode = bitStream.Read8(BlockFlags.Length - 1);
+		
+		if (BlockFlags[mode] == 1)
             return 2048;
 
         return 256;
     }
+
+	/// <summary>
+	/// Read a string based on the current stream and bit position and the <see cref="BitStream"/> encoding
+	/// </summary>
+	/// <param name="length">Length of the string to read</param>
+	private static string ReadString(BitStream<ArrayByteStream> bitStream, int length)
+	{
+        Span<byte> span = new byte[length];
+        bitStream.Read(span);
+		return Encoding.ASCII.GetString(span);
+	}
 }
